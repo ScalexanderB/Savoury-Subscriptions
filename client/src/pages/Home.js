ca5abcfbe18697054451e8b33c1693255ad5db5a
Dave Quinn <dquinn8@cogeco.ca>
2022-01-21 12:24:44 -0500

    Updated Nav component added some css and put in some color so it looks like at least... something

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,15 +1,27 @@
 import React from "react";
-import CategoryMenu from "../components/CategoryMenu"
-import MealsList from "../components/MealsList";
-import Cart from "../components/Cart";
+import './style.css';
+
+import Header from "../components/Header"
+
 
 const Home = () => {
 
   return (
     <div className="container">
-      <CategoryMenu />
-      <MealsList />
-      <Cart />
+      <Header />
+
+    <div className="bigBannerBlock">
+      <span>Add Meals NOW!!</span>
+    </div>
+
+    <div className="bigBannerBlock">
+      <span>About Us!</span>
+    </div>
+
+    <div className="bigBannerBlock">
+      <span>🤘 Let's Get Savory!! 🤘</span>
+    </div>
+
     </div>
   );
 };
