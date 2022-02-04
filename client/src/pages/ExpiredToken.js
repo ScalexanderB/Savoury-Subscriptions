5aab69ebbf15c4512f13eee44db40b9510b6e01b
Dave Quinn <dquinn8@cogeco.ca>
2022-02-04 00:52:27 -0500

    personalized 404 page

--- client/src/pages/ExpiredToken.js
+++ client/src/pages/ExpiredToken.js
@@ -5,7 +5,7 @@ const Expired = () => {
     <div className="d-flex flex-column justify-center" style={{textAlign:"center"}}> 
         <h1>Your Session Has Expired</h1>
         <h1>
-          <img src="https://www.svgrepo.com/show/356803/expired.svg" style={{width:"15rem",height:"15rem"}}/>
+          <img src="https://www.svgrepo.com/show/356803/expired.svg" alt="Session has expired" style={{width:"15rem",height:"15rem"}}/>
         </h1>
         <h3>Please Login Again...</h3>
         <p>&nbsp;</p>
